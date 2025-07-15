3563ed1bec66806fe478bdb416ee18b36f3c07dd
Aliyyah <91733372+aliyyah-u@users.noreply.github.com>
2025-07-15 12:39:27 +0100

    Update settings.py

--- my_final_project/my_final_project/settings.py
+++ my_final_project/my_final_project/settings.py
@@ -258,6 +258,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "biswas_agro/static"]
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
